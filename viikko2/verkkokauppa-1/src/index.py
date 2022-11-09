3e4fa281f9ff9b44bd5c09d9753a1f182192eb66
Vili Sirkeoja <vili.sirkeoja@helsinki.fi>
2022-11-09 22:49:57 +0200

    Week 2 exercise 11

--- viikko2/verkkokauppa-1/src/index.py
+++ viikko2/verkkokauppa-1/src/index.py
@@ -1,15 +1,21 @@
+from kirjanpito import Kirjanpito
 from kauppa import Kauppa
 from varasto import Varasto
 from pankki import Pankki
 from viitegeneraattori import Viitegeneraattori
-from kirjanpito import Kirjanpito
 
 
 def main():
-    varasto = Varasto.get_instance()
-    pankki = Pankki.get_instance()
-    viitegeneraattori = Viitegeneraattori.get_instance()
+    kirjanpito = Kirjanpito()
+    varasto = Varasto(kirjanpito)
+    pankki = Pankki(kirjanpito)
+    viitegeneraattori = Viitegeneraattori()
     kauppa = Kauppa(varasto, pankki, viitegeneraattori)
+
+    #varasto = Varasto.get_instance()
+    #pankki = Pankki.get_instance()
+    #viitegeneraattori = Viitegeneraattori.get_instance()
+    #kauppa = Kauppa(varasto, pankki, viitegeneraattori)
 
     # kauppa hoitaa yhden asiakkaan kerrallaan seuraavaan tapaan:
     kauppa.aloita_asiointi()
@@ -28,7 +34,7 @@ def main():
     kauppa.tilimaksu("Arto Vihavainen", "3425-1652")
 
     # kirjanpito
-    for tapahtuma in Kirjanpito.get_instance().tapahtumat:
+    for tapahtuma in kirjanpito.tapahtumat:
         print(tapahtuma)
 
 
