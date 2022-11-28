1dff20049acd60dc1c01ad50505cc01a90cf767c
Vili Sirkeoja <vili.sirkeoja@helsinki.fi>
2022-11-29 01:56:17 +0200

    Update function lisaa_tuote in ostoskori.py

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -6,6 +6,7 @@ class Ostoskori:
         self._hinta = 0
         self._tavaroita_korissa = 0
         self._ostokset = []
+        self._tuotteet = []
 
     def tavaroita_korissa(self):
         return self._tavaroita_korissa
@@ -14,8 +15,15 @@ class Ostoskori:
         return self._hinta
 
     def lisaa_tuote(self, lisattava: Tuote):
-        ostos = Ostos(lisattava)
-        self._ostokset.append(ostos)
+        if lisattava.nimi() in self._tuotteet:
+            for ostos in self._ostokset:
+                if ostos.tuotteen_nimi() == lisattava.nimi():
+                    ostos.muuta_lukumaaraa(1)
+        else:
+            ostos = Ostos(lisattava)
+            self._ostokset.append(ostos)
+            self._tuotteet.append(ostos.tuotteen_nimi())
+
         self._hinta += lisattava.hinta()
         self._tavaroita_korissa += 1
 
